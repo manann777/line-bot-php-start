116f97424e94dd4ee439688caa1ba4e7da90af5a
manann777 <imanann777@gmail.com>
2017-04-25 15:05:18 +0700

    move url

--- bot.php
+++ bot.php
@@ -28,6 +28,7 @@ function textreply($text){
 
 
 function sender($post,$access_token){
+	$url = 'https://api.line.me/v2/bot/message/reply';
 	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
@@ -67,7 +68,7 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
